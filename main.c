7509753ea922e69ee30edf6ec12088642f41d7de
Samuel Maximus <samuellamere@gmail.com>
2022-11-22 10:29:18 +0700

    tes

--- main.c
+++ main.c
@@ -4,36 +4,40 @@
 
 int main_process();
 
-void convCalc(){
-    int val1; char operator;
-    printf("Input value: \n");scanf("%d",&val1);
+void convCalc()
+{
+    int val1;
+    char operator;
+    printf("Input value: \n");
+    scanf("%d", &val1);
     while (1)
-    {   
-        printf("Input operator (+)(-)(*)(/)(%): \n");scanf("%c",&operator);
-        if (operator=='=')
+    {
+        printf("Input operator (+)(-)(*)(/)(%): \n");
+        scanf("%c", &operator);
+        if (operator== '=')
         {
             printf("%d\n", val1);
             break;
         }
-        
+
         int val2;
         switch (operator)
         {
         case '+':
-            scanf("%d",&val2);
-            val1+=val2;
+            scanf("%d", &val2);
+            val1 += val2;
         case '-':
-            scanf("%d",&val2);
-            val1-=val2;
+            scanf("%d", &val2);
+            val1 -= val2;
         case '/':
-            scanf("%d",&val2);
-            val1/=val2;
+            scanf("%d", &val2);
+            val1 /= val2;
         case '*':
-            scanf("%d",&val2);
-            val1*=val2;
+            scanf("%d", &val2);
+            val1 *= val2;
         case '%':
-            scanf("%d",&val2);
-            val1%=val2;
+            scanf("%d", &val2);
+            val1 %= val2;
         default:
             printf("Invalid input, calculator will exit.\n");
             main_process();
@@ -41,31 +45,34 @@ void convCalc(){
     }
 }
 
-void factorialCalc(){
-    
+void factorialCalc()
+{
 }
 
-void deretCalc(){
+void deretCalc()
+{
     printf("Work In Progress\n");
     main_process();
 }
 
-void printHelp(){
+void printHelp()
+{
     printf("Help\n");
     main_process();
 }
 
-int main_process(){
+int main_process()
+{
     printf("Pilih Salah Satu Opsi Berikut: (Ketikkan Angka Depan Opsi)\n");
     printf("1. Kalkulator Konvensional (Tambah Kurang Kali Bagi Modulus)\n");
     printf("2. Faktorial (Input 1 angka untuk mendapatkan faktorialnya.)\n");
     printf("3. Kalkulator deret aritmatika/geometri.\n");
     printf("4. Help\n");
     printf("5. Exit\n");
-    
+
     int selectionInput;
     scanf("%d", &selectionInput);
-    
+
     switch (selectionInput)
     {
     case 1:
@@ -87,7 +94,8 @@ int main_process(){
     }
 }
 
-int main(){
+int main()
+{
     main_process();
     return 0;
-}
+}=======
